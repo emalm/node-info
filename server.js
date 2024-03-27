bb287284aef8e7b401def92f1bc2f413870945a6
Eric Malm <emalm@vmware.com>
2024-03-27 01:26:49 -0700

    Formatting in server script

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ HOST = null;
 
 var host = process.env.CF_INSTANCE_INTERNAL_IP || "0.0.0.0";
 var port = process.env.PORT || 3000;
-var cell_ip = process.env.CF_INSTANCE_IP || "(no external CF IP)";
+
 var index = process.env.CF_INSTANCE_INDEX || "(no index)";
+var cell_ip = process.env.CF_INSTANCE_IP || "(no external CF IP)";
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
